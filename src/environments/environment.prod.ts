9d0dd376cb9753e6bc22c855be7e9a14db92a2ac
robertocannella <robertocannella@gmail.com>
2022-03-17 22:11:55 -0400

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'aidatapipes',
+    appId: '1:652694324617:web:54cc14cd127dfbe78b4d58',
+    storageBucket: 'aidatapipes.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAnNoD1_dRa-_xMq_0AJjVim8um4tpF1xk',
+    authDomain: 'aidatapipes.firebaseapp.com',
+    messagingSenderId: '652694324617',
+    measurementId: 'G-DYBLNL77QE',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'aidatapipes',
+    appId: '1:652694324617:web:54cc14cd127dfbe78b4d58',
+    storageBucket: 'aidatapipes.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAnNoD1_dRa-_xMq_0AJjVim8um4tpF1xk',
+    authDomain: 'aidatapipes.firebaseapp.com',
+    messagingSenderId: '652694324617',
+    measurementId: 'G-DYBLNL77QE',
+  },
   production: false
 };
 
