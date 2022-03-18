a8218169ba09cadbb6661ca84fa4e9564d32160a
robertocannella <robertocannella@gmail.com>
2022-03-17 22:05:11 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ai-datapipes';
+  //items: Observable<any[]>;
+  constructor(public firestore: AngularFirestore) {
+    //this.items = firestore.collection('datapipes').valueChanges();
+  }
+  ngOnInit() {
+    this.getData();
+  }
+
+
+  // data and firestore
+
+  public data: any[] = [];
+
+  update(data: any) {
+    console.log("data", data)
+  }
+
+  getData() {
+    this.firestore.collection('datapipes').snapshotChanges().subscribe((res: any) => {
+      res.forEach((change: any) => {
+        const doc = { ...change.payload.doc.data(), id: change.payload.doc.id } // create new object with ID field from firestore
+
+        switch (change.type) {
+          case 'added':
+            this.data.push(doc)
+            break;
+          case 'modified':
+            const index = this.data.findIndex((item) => item.id == doc.id) // get the item from data []
+            this.data[index] = doc; // overwrite old element with the modified one
+            break;
+          case 'removed':
+            this.data = this.data.filter((item) => item.id !== doc.id) // filter out the removed element as new array
+            break;
+          default: // default case required
+            break;
+        }
+      });
+      this.update(this.data);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
