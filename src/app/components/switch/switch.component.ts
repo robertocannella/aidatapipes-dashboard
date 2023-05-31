9c1dccac538e8d7ef2e8623c4696bd4810587cc7
robertocannella <robertocannella@gmail.com>
2023-05-31 17:30:05 -0400

    Add timeout to sprinkler button

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -8,15 +8,23 @@ import { SprinklerStatusService } from 'src/app/services/sprinkler-status.servic
 })
 export class SwitchComponent implements OnInit {
 
-  status= false;
+  status: boolean = false;
+  isButtonDisabled: boolean = false;
   constructor(private sprinklerService: SprinklerStatusService ) { }
 
   ngOnInit(): void {
   }
   onToggle(): void {
+    this.disableButton();
     this.status = !this.status;
     this.sprinklerService.setSprinklerStatus(this.status)
 
+  }
+  disableButton(): void {
+    this.isButtonDisabled = true;
+    setTimeout(() => {
+      this.isButtonDisabled = false;
+    }, 5000); // 5000 milliseconds = 5 seconds
   }
 
 }
