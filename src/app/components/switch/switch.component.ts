2edd476540311e6da2088f03dee857cc394f6ddf
robertocannella <robertocannella@gmail.com>
2024-05-26 13:09:30 -0400

    Add Timer Button

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -24,23 +24,23 @@ export class SwitchComponent implements OnInit {
   constructor(
     private sprinklerService: SprinklerStatusService,
     private notificationService: NotificationService,
-    private storageService: StorageService ) { 
-    
+    private storageService: StorageService) {
+
     // Which file contains the info? Path is to oot of storage bucket
     this.storageService.setRef('sprinkler_event.txt');
 
-    this.subs$ = this.sprinklerService.getCurrentSprinklerStatus().subscribe((data: any)=>{
+    this.subs$ = this.sprinklerService.getCurrentSprinklerStatus().subscribe((data: any) => {
       this.status = data.isOn;
     });
-    this.subs3$ = this.notificationService.getUserList().subscribe((data:any)=>{
+    this.subs3$ = this.notificationService.getUserList().subscribe((data: any) => {
       this.users = data.email;
     })
-    
+
 
   }
 
 
-  ngOnDestroy():void {
+  ngOnDestroy(): void {
     if (this.subs$) {
       this.subs$.unsubscribe();
     }
@@ -48,38 +48,38 @@ export class SwitchComponent implements OnInit {
       this.subs2$.unsubscribe();
     }
   }
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.getFileContents();
 
-    this.subs2$ = this.sprinklerService.getSchedules().subscribe((events)=>{
+    this.subs2$ = this.sprinklerService.getSchedules().subscribe((events) => {
       this.events = []
-        events.forEach(event => {
-          this.events.push(event as SchedulerGroup)
-        })
+      events.forEach(event => {
+        this.events.push(event as SchedulerGroup)
+      })
     });
-    this.subs3$ = this.notificationService.getUserList().subscribe((data:any)=>{
+    this.subs3$ = this.notificationService.getUserList().subscribe((data: any) => {
       this.users = []
-      if (data.email){
-        data.email.forEach((user:NotificationUser)=>{
-            this.users.push(user)
+      if (data.email) {
+        data.email.forEach((user: NotificationUser) => {
+          this.users.push(user)
         })
       }
     })
   }
 
-   appendFile(content: string){
+  appendFile(content: string) {
     this.storageService.appendFile(content);
-   }
-   async getFileContents(){
+  }
+  async getFileContents() {
     this.fileContents = await this.storageService.getContents();
-    
-   }
+
+  }
   onToggle(): void {
     this.disableButton();
-   
+
     this.status = !this.status;
     this.sprinklerService.setSprinklerStatus(this.status)
-    this.appendFile('Time: ' + Date.now() + '\t' + 'Status: ' +  this.status.toString() + '\n');
+    this.appendFile('Time: ' + Date.now() + '\t' + 'Status: ' + this.status.toString() + '\n');
 
   }
   disableButton(): void {
@@ -87,6 +87,10 @@ export class SwitchComponent implements OnInit {
     setTimeout(() => {
       this.isButtonDisabled = false;
     }, 5000); // 5000 milliseconds = 5 seconds
+  }
+
+  showTimer(): void {
+    console.log("SHOWING TIMER")
   }
 
 }
